42d4687113f71562a882c110b5c7d06de9152fc5
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-11 21:14:43 +0100

    La til repsonsiv nav bar

--- fleksitid-k/fleksitid/app/admin/layout.js
+++ fleksitid-k/fleksitid/app/admin/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 
-import BasicTabs from '../../components/adminNavbar'
-
+import ResponsiveAppBar from '../../components/adminResponsivNavBar';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <BasicTabs />
+      <ResponsiveAppBar />
       {children}</body>
     </html>
   )
