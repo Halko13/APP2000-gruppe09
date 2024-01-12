4a1a3a17f2be5a12fcf16f4ba96f4fd54c82a1a8
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-12 19:56:28 +0100

    La til validering på knapp for ny bruker skjema

--- fleksitid-k/fleksitid/components/nyBrukerButton.js
+++ fleksitid-k/fleksitid/components/nyBrukerButton.js
@@ -2,13 +2,21 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-export default function IconLabelButtons() {
+export default function IconLabelButtons({ onSave, isFormValid, onFormReset }) {
+  const handleClick = () => {
+    if (isFormValid) {
+      onSave();
+      onFormReset(); // Reset the form after successful save
+    } else {
+      // Show an error message or handle the case where the form is not valid
+      console.log("valider ikke " + isFormValid)
+      alert('Please fill in all required fields and ensure passwords match.');
+    }
+  };
+
   return (
     <Stack direction="row" spacing={2} alignItems="flex-end">
-      <Button variant="contained" onClick={() => {
-            // Når blir klikket på
-            alert('clicked');
-        }}>
+      <Button variant="contained" onClick={handleClick}>
         Send
       </Button>
     </Stack>
