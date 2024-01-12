d87492f367c3676d75486ac5de48cb1c7e756f51
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-12 19:56:52 +0100

    La til validering i ny bruker skjema

--- fleksitid-k/fleksitid/components/nyBrukerSkjema.js
+++ fleksitid-k/fleksitid/components/nyBrukerSkjema.js
@@ -1,12 +1,11 @@
+// nyBrukerSkjema.js
 'use client';
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-
-/* Importer textfields */
-import FormPropsTextFields from "../components/adminANR-TF";
-import IconLabelButtons from "../components/nyBrukerButton";
+import FormPropsTextFields from "@/components/nyBrukerTextFields";
+import IconLabelButtons from "@/components/nyBrukerButton";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,21 +15,69 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-export default function CSSGrid() {
+export default function NyBrukerSkjema() {
+  const [formData, setFormData] = React.useState({
+    AnsattNr: '',
+    Fornavn: '',
+    Etternavn: '',
+    Stilling: '',
+    antallJobbtimer: '',
+    password: '',
+    gjentaPassword: '',
+  });
+
+  const handleSave = () => {
+    // Here you can perform the logic to save the data to a database
+    console.log('Saving data to the database:', formData);
+
+    // Reset the form data after saving
+    setFormData({
+      AnsattNr: '',
+      Fornavn: '',
+      Etternavn: '',
+      Stilling: '',
+      antallJobbtimer: '',
+      password: '',
+      gjentaPassword: '',
+    });
+  };
+
+  const handleFormReset = () => {
+    // Reset the form data
+    setFormData({
+      AnsattNr: '',
+      Fornavn: '',
+      Etternavn: '',
+      Stilling: '',
+      antallJobbtimer: '',
+      password: '',
+      gjentaPassword: '',
+    });
+  };
+
+  const isFormValid =
+    formData.AnsattNr !== '' &&
+    formData.Fornavn !== '' &&
+    formData.Etternavn !== '' &&
+    formData.Stilling !== '' &&
+    formData.password !== '' &&
+    formData.gjentaPassword !== '' &&
+    formData.password === formData.gjentaPassword;
+
   return (
     <Box sx={{ width: 0.5 }} alignItems={'center'} style={{ margin: 'auto' }}>
       <Box display="grid" gridTemplateColumns="repeat(1fr, 1fr)" gap={2} alignItems={'center'} style={{ margin: 'auto' }}>
         <Box gridColumn="span 1">
           <Item>
-            <FormPropsTextFields />
+            <FormPropsTextFields formData={formData} onChange={setFormData} />
           </Item>
         </Box>
         <Box gridColumn="span 1">
-  <Item>
-    <IconLabelButtons />
-  </Item>
-</Box>  
+          <Item>
+            <IconLabelButtons onSave={handleSave} isFormValid={isFormValid} onFormReset={handleFormReset} />
+          </Item>
         </Box>
       </Box>
+    </Box>
   );
 }
