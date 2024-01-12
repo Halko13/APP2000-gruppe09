98be52a6a204a2d187d30fda65a507d071b864df
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-12 19:54:45 +0100

    Rette opp app layout

--- fleksitid-k/fleksitid/app/layout.js
+++ fleksitid-k/fleksitid/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import BasicTabs from '../components/adminNavbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      {/* <BasicTabs /> */}
       {children}</body>
     </html>
   )
