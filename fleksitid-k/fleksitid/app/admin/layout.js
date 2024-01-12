accd7e2f064907654da03a4ece1234210c3cdd32
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-12 19:54:10 +0100

    Finere import setning

--- fleksitid-k/fleksitid/app/admin/layout.js
+++ fleksitid-k/fleksitid/app/admin/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 
-import ResponsiveAppBar from '../../components/adminResponsivNavBar';
+import ResponsiveAppBar from '@/components/adminResponsivNavBar';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
