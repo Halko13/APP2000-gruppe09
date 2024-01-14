b35e34d1743a1da2685408404efd1e4740d612ab
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-14 16:15:28 +0100

    Separerte hooks

--- fleksitid-k/fleksitid/components/nyBrukerTextFields.js
+++ fleksitid-k/fleksitid/components/nyBrukerTextFields.js
@@ -4,33 +4,18 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useFormValidation } from "@/hooks/useFormValidation";
 import { useFormUpdate } from "@/hooks/useFormUpdate";
+import { useFormDataEffect } from "@/hooks/useFormDataEffect";
+import { usePasswordChange } from "@/hooks/usePasswordChange";
+import { useGjentaPasswordChange } from "@/hooks/useGjentaPasswordChange";
 
 export const PASSWORD_LENGTH = 6;
 
 export default function FormTextFields({ formData, onChange }) {
   const handleChange = useFormUpdate(onChange);
   const passwordError = useFormValidation(formData);
-
-  const handlePasswordChange = (event) => {
-    const { id, value } = event.target;
-    const isPasswordValid = /^[0-9]*$/.test(value);
-    onChange((prevData) => ({ ...prevData, [id]: isPasswordValid ? value : '' }));
-  };
-
-  const handleGjentaPasswordChange = (event) => {
-    const { id, value } = event.target;
-    onChange((prevData) => ({ ...prevData, [id]: value }));
-  };
-
-  React.useEffect(() => {
-    document.getElementById('AnsattNr').value = formData.AnsattNr;
-    document.getElementById('Fornavn').value = formData.Fornavn;
-    document.getElementById('Etternavn').value = formData.Etternavn;
-    document.getElementById('Stilling').value = formData.Stilling;
-    document.getElementById('antallJobbtimer').value = formData.antallJobbtimer;
-    document.getElementById('password').value = formData.password;
-    document.getElementById('gjentaPassword').value = formData.gjentaPassword;
-  }, [formData]);
+  useFormDataEffect(formData);
+  const handlePasswordChange = usePasswordChange(onChange);
+  const handleGjentaPasswordChange = useGjentaPasswordChange(onChange);
 
   return (
     <Box
