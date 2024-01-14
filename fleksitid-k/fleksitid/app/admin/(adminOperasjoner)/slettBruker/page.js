f7e4e844cfbde5af2016b410cf7df12bd051cc39
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-14 11:18:32 +0100

    La til slett bruker side

---
+++ fleksitid-k/fleksitid/app/admin/(adminOperasjoner)/slettBruker/page.js
@@ -0,0 +1,12 @@
+import NyBrukerSkjema from "@/components/nyBrukerSkjema";
+
+export default function admin() {
+  return (
+    <main>
+      <div>
+        hello world! admin - slettBruker
+        
+      </div>
+    </main>
+  );
+}
