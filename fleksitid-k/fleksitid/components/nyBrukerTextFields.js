234ec4176e1cac5fdee1c2850f75cbee831a40a3
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-14 16:03:04 +0100

    Separerte hooks

--- fleksitid-k/fleksitid/components/nyBrukerTextFields.js
+++ fleksitid-k/fleksitid/components/nyBrukerTextFields.js
@@ -2,47 +2,24 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useFormValidation } from "@/hooks/useFormValidation";
+import { useFormUpdate } from "@/hooks/useFormUpdate";
 
 export const PASSWORD_LENGTH = 6;
+
 export default function FormTextFields({ formData, onChange }) {
-  const [passwordError, setPasswordError] = React.useState(false);
-
-  const isNumeric = (value) => /^[0-9]+$/.test(value);
-
-  React.useEffect(() => {
-    const isValid =
-      formData.AnsattNr &&
-      formData.Fornavn &&
-      formData.Etternavn &&
-      formData.Stilling &&
-      isNumeric(formData.password) &&
-      isNumeric(formData.gjentaPassword) &&
-      formData.password === formData.gjentaPassword &&
-      formData.password.length === PASSWORD_LENGTH;
-
-    setPasswordError(!isValid);
-  }, [formData]);
-
-  const handleChange = (event) => {
-    const { id, value } = event.target;
-    onChange((prevData) => ({ ...prevData, [id]: value }));
-    setPasswordError(false);
-  };
+  const handleChange = useFormUpdate(onChange);
+  const passwordError = useFormValidation(formData);
 
   const handlePasswordChange = (event) => {
     const { id, value } = event.target;
-    //Sjekkker om det er tall mellom 0 og 9
     const isPasswordValid = /^[0-9]*$/.test(value);
-    //Setter password er ugyldig hvis det ikke er tall og ikke riktig lengde
-    setPasswordError(!isPasswordValid || value.length !== PASSWORD_LENGTH);
     onChange((prevData) => ({ ...prevData, [id]: isPasswordValid ? value : '' }));
   };
 
   const handleGjentaPasswordChange = (event) => {
     const { id, value } = event.target;
-    const isPasswordValid = /^[0-9]*$/.test(value);
-    setPasswordError(!isPasswordValid || value !== formData.password);
-    onChange((prevData) => ({ ...prevData, [id]: isPasswordValid ? value : '' }));
+    onChange((prevData) => ({ ...prevData, [id]: value }));
   };
 
   React.useEffect(() => {
@@ -60,7 +37,7 @@ export default function FormTextFields({ formData, onChange }) {
       component="form"
       sx={{
         '& .MuiTextField-root': { m: 1, width: 1 },
-        mx: 2, // Set equal margin on both sides
+        mx: 2,
       }}
       noValidate
       autoComplete="off"
@@ -106,7 +83,7 @@ export default function FormTextFields({ formData, onChange }) {
           onChange={handleChange}
           value={formData.antallJobbtimer}
         />
-        <TextField
+         <TextField
           required
           id="password"
           label="Password"
