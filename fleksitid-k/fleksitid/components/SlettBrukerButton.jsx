32f3675c0abe7d67cc73a532b7dfcc7328fb78a5
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-15 21:03:43 +0100

    Refaktoring

--- fleksitid-k/fleksitid/components/SlettBrukerButton.jsx
+++ fleksitid-k/fleksitid/components/SlettBrukerButton.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-export default function SlettBrukerButton({ onSave, isFormValid, onFormReset }) {
-  const handleSaveClick = () => {
+export default function SlettBrukerButton({ onDelete, isFormValid, onFormReset }) {
+  const handleDeleteUser = () => {
     if (isFormValid) {
-      onSave();
+      onDelete();
       onFormReset(); // Reset the form after successful save
     } else {
       // Show an error message or handle the case where the form is not valid
       console.log("valider ikke " + isFormValid);
-      alert('Please fill in all required fields and ensure passwords match.');
+      alert('Please fill in the field');
     }
   };
 
@@ -21,7 +21,7 @@ export default function SlettBrukerButton({ onSave, isFormValid, onFormReset }) 
 
   return (
     <Stack direction="row" spacing={2} justifyContent="flex-end">
-      <Button variant="contained" onClick={handleSaveClick}>
+      <Button variant="contained" onClick={handleDeleteUser}>
         Slett bruker
       </Button>
       <Button variant="outlined" onClick={handleResetClick}>
