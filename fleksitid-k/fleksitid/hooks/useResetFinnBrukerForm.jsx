b3503328d137c67fd163b7c2f8f46bf4cb1fb9a2
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-15 21:49:54 +0100

    Fjernet ubrukte filer

--- fleksitid-k/fleksitid/hooks/useResetFinnBrukerForm.jsx
+++
@@ -1,8 +0,0 @@
-// useFormDataEffect.js
-import * as React from 'react';
-
-export const useResetFinnBrukerForm = (formData) => {
-  React.useEffect(() => {
-    document.getElementById('AnsattNr').value = formData.AnsattNr;
-  }, [formData]);
-};
