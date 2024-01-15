e51c0ee7c29d9aeb064f0ebc5953b0950822ea4c
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-15 18:58:25 +0100

    La til slett bruker page

--- fleksitid-k/fleksitid/app/admin/(adminOperasjoner)/slettBruker/page.js
+++ fleksitid-k/fleksitid/app/admin/(adminOperasjoner)/slettBruker/page.js
@@ -1,11 +1,16 @@
-import NyBrukerSkjema from "@/components/nyBrukerSkjema";
+import React from 'react';
+import SlettBrukerSkjema from '@/components/SlettBrukerSkjema';
+import SlettBrukerTittel from '@/components/SlettBrukerTittel'; // Corrected import
+import { Box } from '@mui/material';
 
 export default function admin() {
   return (
     <main>
       <div>
-        hello world! admin - slettBruker
-        
+        <Box>
+          <SlettBrukerTittel />
+          <SlettBrukerSkjema />
+        </Box>
       </div>
     </main>
   );
