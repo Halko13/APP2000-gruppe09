0f03671d75a84e773cbfe44bc836d03e8de9a13d
Toffern96 <christopher@westergren.no>
2024-01-17 14:47:04 +0100

    Klargjort side, samt satt på navbar

---
+++ fleksitid-k/fleksitid/app/sjekkinn/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResponsiveAppBar from '../components/NavBar'
+
+const Knapp = () => {
+  return (
+    <div>
+        <ResponsiveAppBar />
+    </div>
+  )
+}
+
+export default Knapp
