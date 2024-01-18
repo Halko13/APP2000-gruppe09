37084644f0cf4326dc585d54f2d1996f3ca2d953
Toffern96 <christopher@westergren.no>
2024-01-18 13:51:42 +0100

    Eventhandler på knapp

--- fleksitid-k/fleksitid/app/components/SjekkinnKnapp.jsx
+++ fleksitid-k/fleksitid/app/components/SjekkinnKnapp.jsx
@@ -1,13 +1,18 @@
+"use client";
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import styles from '../page.module.css'
 
 const SjekkinnKnapp = () => {
+  let inn = "SJEKK INN"
+  let ut = "SJEKK UT"
   return (
     <div className={styles.knapp}>
-    <Button variant="contained" size="large" >
-          SJEKK INN
+    <Button variant="contained" size="large" onClick={() => {
+    alert('clicked');
+  }}>
+          {inn}
         </Button>
     </div>
   )
