c62a833e4c65dc021614b4dbffd17f43faa89dfe
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-22 20:47:23 +0100

    Refaktorer funksjon navn

--- fleksitid-k/fleksitid/components/nyBrukerButton.jsx
+++ fleksitid-k/fleksitid/components/nyBrukerButton.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-export default function IconLabelButtons({ onSave, isFormValid, onFormReset }) {
+export default function NyBrukerButton({ onSave, isFormValid, onFormReset }) {
   const handleSaveClick = () => {
     if (isFormValid) {
       onSave();
@@ -10,7 +10,7 @@ export default function IconLabelButtons({ onSave, isFormValid, onFormReset }) {
     } else {
       // Show an error message or handle the case where the form is not valid
       console.log("valider ikke " + isFormValid);
-      alert('Fyll inn alle felter og passord må matche');
+      alert("Fyll inn alle felter og passord må matche");
     }
   };
 
