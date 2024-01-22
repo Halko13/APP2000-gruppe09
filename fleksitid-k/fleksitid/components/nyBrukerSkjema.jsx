0b608f7bf01c7e1210f52fab4fa8202d1b936e1a
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-22 13:56:31 +0100

    La til sjekk, hvis ansattNummer finnes fra før

--- fleksitid-k/fleksitid/components/nyBrukerSkjema.jsx
+++ fleksitid-k/fleksitid/components/nyBrukerSkjema.jsx
@@ -7,7 +7,7 @@ import NyBrukerForm from "@/components/nyBrukerTextFields";
 import NyBrukerButton from "@/components/nyBrukerButton";
 import { PASSWORD_LENGTH } from "@/components/nyBrukerTextFields";
 import { db } from '@/app/firebaseConfig';
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc, getDoc } from "firebase/firestore"; 
 
 export default function NyBrukerSkjema() {
   const [formData, setFormData] = React.useState({
@@ -23,17 +23,33 @@ export default function NyBrukerSkjema() {
 
   const handleSave = async () => {
     console.log('Saving data to the database:',  formData);
+    // Sjekke om finnes fra før
+        
+    const docRef = doc(db, "Brukere", formData.AnsattNr);
+    const docSnap = await getDoc(docRef);
+  
+  if (docSnap.exists()) {
+    alert("Ansatt nummer finnes fra før");
+  } else {
+    // docSnap.data() will be undefined in this case
     await setDoc(doc(db, "Brukere", formData.AnsattNr), {
       AnsattNr: formData.AnsattNr,
       Fornavn: formData.Fornavn,
       Etternavn: formData.Etternavn,
+      Stilling: formData.Stilling,
       AntallJobbTimer: formData.AntallJobbtimer,
       Password: formData.Password,
       Innlogget: false,
       ErAdmin: formData.ErAdmin,
     });
-
     alert("Ny bruker lagt til");
+  } 
+
+
+
+
+
+ 
     // Reset the form data after saving
     handleFormReset();
   };
