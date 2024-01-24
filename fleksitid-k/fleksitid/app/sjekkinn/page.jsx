75a071abdb9038bfb981a5f891f81809f20a8863
Toffern96 <christopher@westergren.no>
2024-01-24 10:08:46 +0100

    fiksa error på klokka

--- fleksitid-k/fleksitid/app/sjekkinn/page.jsx
+++ fleksitid-k/fleksitid/app/sjekkinn/page.jsx
@@ -1,3 +1,5 @@
+// Forfatter Christopher Westergren
+
 import React from 'react'
 import ResponsiveAppBar from '../components/NavBar'
 import styles from '../page.module.css'
@@ -12,7 +14,9 @@ const SjekkInn = () => {
         <ResponsiveAppBar />
         <h1 className={styles.velkommen}>Velkommen {navn}! </h1>
         <h1 className={styles.dato}>Dagens dato: {dato}</h1>
-        <Klokke />
+        {/* Bruker suppressHydrationWarning for å unngå feilmelding
+        om at tiden på klientsiden og serversiden ikke stemmer! */}
+        <Klokke suppressHydrationWarning />
         <h1 className={styles.ov}>Gjenværende tid i dag: 7:59</h1>
         <SjekkinnKnapp />
     </>
