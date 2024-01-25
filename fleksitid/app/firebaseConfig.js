00e973ca176d4d629740e4d13ee59a6dbd578593
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-25 13:09:25 +0100

    firebase funker ikke

--- fleksitid/app/firebaseConfig.js
+++ fleksitid/app/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXAV7xZr-ztj97FSL3oOhaUqsIiqfR2fk",
-  authDomain: "visma-5c70e.firebaseapp.com",
-  projectId: "visma-5c70e",
-  storageBucket: "visma-5c70e.appspot.com",
-  messagingSenderId: "161477454641",
-  appId: "1:161477454641:web:7abeb57872fb86ef719e10",
+  apiKey: process.env.FIRESTORE_API_KEY,
+  authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRESTORE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
