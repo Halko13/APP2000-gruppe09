115c25922f1812e2a9770785f20b33fbc44426ef
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-25 11:55:17 +0100

    Fjernet mappe

--- fleksitid-k/fleksitid/app/admin/page.js
+++
@@ -1,9 +0,0 @@
-// Utviklet av Halvor Vilnes
-
-export default function admin() {
-  return (
-    <main>
-      <div>hello world! admin</div>
-    </main>
-  );
-}
--- fleksitid-k/fleksitid/hooks/useResetFinnBrukerForm.jsx
+++
@@ -1,9 +0,0 @@
-// Utviklet av Halvor Vilnes
-
-import * as React from "react";
-
-export const useResetFinnBrukerForm = (formData) => {
-  React.useEffect(() => {
-    document.getElementById("AnsattNr").value = formData.AnsattNr;
-  }, [formData]);
-};
