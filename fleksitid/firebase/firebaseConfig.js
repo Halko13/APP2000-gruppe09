226dccb6fb5e6249227c9fe5e8e994f470786b36
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-26 11:59:32 +0100

    la inn env variabler, får permission error fra firestore

--- fleksitid/firebase/firebaseConfig.js
+++ fleksitid/firebase/firebaseConfig.js
@@ -9,6 +9,15 @@ const firebaseConfig = {
   messagingSenderId: "161477454641",
   appId: "1:161477454641:web:7abeb57872fb86ef719e10",
 };
+//
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIRESTORE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIRESTORE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIRESTORE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIRESTORE_APP_ID,
+// };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
