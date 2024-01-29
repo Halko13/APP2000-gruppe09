b2223c6a911d64a4db46732a1b98f4f81b30b668
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-01-29 11:26:44 +0100

    Refaktoring

---
+++ fleksitid/app/dashboard/historikk/page.js
@@ -0,0 +1,15 @@
+// Utviklet av Halvor Vilnes
+
+import React from "react";
+import { Box } from "@mui/material";
+// Komponenter
+
+export default function Admin() {
+  return (
+    <main>
+      <div>
+        <Box>hei</Box>
+      </div>
+    </main>
+  );
+}
