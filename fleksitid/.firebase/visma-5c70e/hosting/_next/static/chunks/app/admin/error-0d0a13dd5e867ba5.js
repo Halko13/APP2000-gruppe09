d0df45072c08d3ad182378da83741de25b48f2ee
SythNorse <124358593+SythNorse@users.noreply.github.com>
2024-03-18 11:13:57 +0100

    firebase hosting

---
+++ fleksitid/.firebase/visma-5c70e/hosting/_next/static/chunks/app/admin/error-0d0a13dd5e867ba5.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[966],{3986:function(e,r,n){Promise.resolve().then(n.bind(n,30890))},30890:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return Error}});var t=n(57437),o=n(2265);function Error(e){let{error:r,reset:n}=e;return(0,o.useEffect)(()=>{console.error(r)},[r]),(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"Noe gikk feil"}),(0,t.jsx)("button",{onClick:()=>n(),children:"Pr\xf8v p\xe5 nytt"})]})}},30622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),f=Symbol.for("react.fragment"),s=Object.prototype.hasOwnProperty,u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,f={},c=null,l=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(l=r.ref),r)s.call(r,t)&&!i.hasOwnProperty(t)&&(f[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===f[t]&&(f[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:l,props:f,_owner:u.current}}r.Fragment=f,r.jsx=q,r.jsxs=q},57437:function(e,r,n){"use strict";e.exports=n(30622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=3986)}),_N_E=e.O()}]);
