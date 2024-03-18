2afaf41b200bb8b3a573fb6b80079907e097a11d
Halvor Vilnes <144660008+Halko13@users.noreply.github.com>
2024-03-18 11:04:40 +0100

    Autorisjon fix

--- fleksitid/components/Autorisasjon/page.js
+++ fleksitid/components/Autorisasjon/page.js
@@ -1,31 +1,61 @@
-'use client';
+// 'use client';
 
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+
+// const forAutorisasjon = (Rettighetersjekk, { hvilkenRolle }) => {
+//     const BeskyttKomponent = (p) => {
+//         const [erAutorisert, settErAutorisert] = useState(false);
+
+//         useEffect(() => {
+//             const rolle = localStorage.getItem('brukerRolle');
+//             if (rolle === hvilkenRolle) {
+//                 settErAutorisert(true);
+//             } else {
+//                 import('next/router').then(({ useRouter }) => {
+//                     const ruter = useRouter();
+//                     ruter.push('/logginn');
+//                 });
+//             }
+//         }, [hvilkenRolle]);
+
+//         if (!erAutorisert) {
+//             return null;
+//         }
+
+//         return <Rettighetersjekk {...p} />;
+//     };
+
+//     return BeskyttKomponent;
+// };
+
+// export default forAutorisasjon;
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router"; // Import useRouter at the top level
 
 const forAutorisasjon = (Rettighetersjekk, { hvilkenRolle }) => {
-    const BeskyttKomponent = (p) => {
-        const [erAutorisert, settErAutorisert] = useState(false);
+  const BeskyttKomponent = (p) => {
+    const [erAutorisert, settErAutorisert] = useState(false);
+    const ruter = useRouter(); // Call useRouter at the top level
 
-        useEffect(() => {
-            const rolle = localStorage.getItem('brukerRolle');
-            if (rolle === hvilkenRolle) {
-                settErAutorisert(true);
-            } else {
-                import('next/router').then(({ useRouter }) => {
-                    const ruter = useRouter();
-                    ruter.push('/logginn'); 
-                });
-            }
-        }, [hvilkenRolle]);
+    useEffect(() => {
+      const rolle = localStorage.getItem("brukerRolle");
+      if (rolle === hvilkenRolle) {
+        settErAutorisert(true);
+      } else {
+        ruter.push("/logginn");
+      }
+    }, []); // Remove hvilkenRolle from the dependency array
 
-        if (!erAutorisert) {
-            return null; 
-        }
+    if (!erAutorisert) {
+      return null;
+    }
 
-        return <Rettighetersjekk {...p} />;
-    };
+    return <Rettighetersjekk {...p} />;
+  };
 
-    return BeskyttKomponent;
+  return BeskyttKomponent;
 };
 
 export default forAutorisasjon;
